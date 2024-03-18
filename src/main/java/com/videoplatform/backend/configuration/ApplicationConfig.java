307874b95c3af549d47dc7ffab5ca7c5be457cb2
mostafak2 <mostafajunayed20@gmail.com>
2024-03-18 03:38:53 -0400

    implemented AuthenticationProvider and PasswordEncoder using BCryptPasswordEncoder

--- src/main/java/com/videoplatform/backend/configuration/ApplicationConfig.java
+++ src/main/java/com/videoplatform/backend/configuration/ApplicationConfig.java
@@ -3,9 +3,13 @@ package com.videoplatform.backend.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.videoplatform.backend.repository.UserRepository;
 
@@ -19,5 +23,22 @@ public class ApplicationConfig {
 	    
 		return username -> repository.findByEmail(username)
 				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+	}
+	
+	
+	// fetch user details and encode password and everything
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+		
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		// TODO Auto-generated method stub
+		return new BCryptPasswordEncoder();
 	}
 }
