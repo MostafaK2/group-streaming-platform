dfb3e8506d0a695a7f018be3a3e5a940c4d46f86
mostafak2 <mostafajunayed20@gmail.com>
2024-03-18 03:42:34 -0400

    Added authentication Manager

--- src/main/java/com/videoplatform/backend/configuration/ApplicationConfig.java
+++ src/main/java/com/videoplatform/backend/configuration/ApplicationConfig.java
@@ -3,8 +3,10 @@ package com.videoplatform.backend.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -34,6 +36,11 @@ public class ApplicationConfig {
 		authProvider.setPasswordEncoder(passwordEncoder());
 		return authProvider;
 		
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 
 	@Bean
