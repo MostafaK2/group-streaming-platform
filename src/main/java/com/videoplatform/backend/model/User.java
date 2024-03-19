8032585bc874165245f61eba194da18c58507fdf
mostafak2 <mostafajunayed20@gmail.com>
2024-03-18 20:35:54 -0400

    added post contruct user initalized values

--- src/main/java/com/videoplatform/backend/model/User.java
+++ src/main/java/com/videoplatform/backend/model/User.java
@@ -21,16 +21,16 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+// remove builder later
 @Entity
-@Table(name = "user")
-@Getter 
-@Setter 
-@NoArgsConstructor 
-@AllArgsConstructor
+@Builder
+@Getter @Setter 
+@NoArgsConstructor @AllArgsConstructor
 public class User implements UserDetails{
 
 	@Id
@@ -111,148 +111,6 @@ public class User implements UserDetails{
 		// TODO Auto-generated method stub
 		return true;
 	}
-	
-	// UserDetails end
-
-	
-	
-	// DO NOT EDIT AFTER THIS: auto generated methods start here
-	public User() {
-		super();
-	}
-
-
-	public User(String userId, String username, String email, String passwordHash, String firstName, String lastName,
-			String profilePicture, List<WatchHistory> watchHistory, List<PartyMember> partyMember) {
-		super();
-		this.userId = userId;
-		this.username = username;
-		this.email = email;
-		this.password = passwordHash;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.profilePicture = profilePicture;
-		this.watchHistory = watchHistory;
-		this.partyMember = partyMember;
-	}
-
-
-	public String getUserId() {
-		return userId;
-	}
-
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-
-	public String getLastName() {
-		return lastName;
-	}
-
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-
-	public String getProfilePicture() {
-		return profilePicture;
-	}
-
-
-	public void setProfilePicture(String profilePicture) {
-		this.profilePicture = profilePicture;
-	}
-
-
-	public List<WatchHistory> getWatchHistory() {
-		return watchHistory;
-	}
-
-
-	public void setWatchHistory(List<WatchHistory> watchHistory) {
-		this.watchHistory = watchHistory;
-	}
-
-
-	public List<PartyMember> getPartyMember() {
-		return partyMember;
-	}
-
-
-	public void setPartyMember(List<PartyMember> partyMember) {
-		this.partyMember = partyMember;
-	}
-
-
-	@Override
-	public String toString() {
-		return "User :\n[\n   " + (userId != null ? "userId = " + userId + ", \n   " : "")
-				+ (username != null ? "username = " + username + ", \n   " : "")
-				+ (email != null ? "email = " + email + ", \n   " : "")
-				+ (password != null ? "passwordHash = " + password + ", \n   " : "")
-				+ (firstName != null ? "firstName = " + firstName + ", \n   " : "")
-				+ (lastName != null ? "lastName = " + lastName + ", \n   " : "")
-				+ (profilePicture != null ? "profilePicture = " + profilePicture + ", \n   " : "")
-				+ (watchHistory != null ? "watchHistory = " + watchHistory + ", \n   " : "")
-				+ (partyMember != null ? "partyMember = " + partyMember : "") + "\n]";
-	}
-
-
-	public UserType getUserType() {
-		return userType;
-	}
-
-
-	public void setUserType(UserType userType) {
-		this.userType = userType;
-	}
-
-
-	public LocalDateTime getUserCreatedAt() {
-		return userCreatedAt;
-	}
-
-
-	public void setUserCreatedAt(LocalDateTime userCreatedAt) {
-		this.userCreatedAt = userCreatedAt;
-	}
-
-
-	
-
 
 	
 }
